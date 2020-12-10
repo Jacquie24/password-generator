b025e4883c3e8b3f0e1007f6164f80ba66f539c0
Jacquie24 <jgcaption@yahoo.com>
2020-12-10 05:36:13 -0500

    Changed prompt to confirm and added if statement

--- script.js
+++ script.js
@@ -21,35 +21,40 @@ function generatePassword(){
   var specialValues = "!@#$%^&*()";
   var passwordGeneratorValue = "";
 
-  var lengthOfPassword = prompt("Enter a number between 8 and 128");
-  if (lengthOfPassword) {
+  // var lengthOfPassword = prompt("Enter a number between 8 and 128");
+  // if (lengthOfPassword) {
 
-  }
+  // }
+  // console.log("Password length is " + lengthOfPassword);
 
-  var useUpperCase = prompt("Would you like to use upper case letters?");
+  var useUpperCase = confirm("Would you like to use upper case letters?");
   if (useUpperCase) {
     var res = passwordGeneratorValue.concat(useUpperCase);
+    console.log("uppercase confirmed");
   }
 
-  var useLowerCase = prompt("Would you like to use lower case letters");
+  var useLowerCase = confirm("Would you like to use lower case letters");
   if (useLowerCase) {
     var res = passwordGeneratorValue.concat(useLowerCase);
+    console.log("lower case confirmed");
   }
 
-  var useNumberValues = prompt("Would you like to use numbers?");
+  var useNumberValues = confirm("Would you like to use numbers?");
   if (useNumberValues) {
     var res = passwordGeneratorValue.concat(useNumberValues);
+    console.log("numbers confirmed");
   }
 
-  var useSpecialValues = prompt("Woule you like to use special characters");
+  var useSpecialValues = confirm("Woule you like to use special characters");
   if (useSpecialValues) {
     var res = passwordGeneratorValue.concat(useSpecialValues);
+    console.log("special characters confirmed");
   }
 
 
-  for (var i = 0; i < lengthOfPassword; i++) {
+  // for (var i = 0; i < lengthOfPassword; i++) {
 
-  }
+  // }
 
 
   return "temporary password";
