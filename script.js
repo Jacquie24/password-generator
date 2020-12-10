aebec40c7c495d12a9f1b6c2858a77f495d060c7
Jacquie24 <jgcaption@yahoo.com>
2020-12-10 09:50:10 -0500

    Changed while loop to if statement

--- script.js
+++ script.js
@@ -15,17 +15,42 @@ function generatePassword(){
 
   // ALL THE LOGIC GOES IN HERE
   // temporary password is a filler2""!
-  var upperCaseValues = "abcdefghijklmnopqrstuvwxyz";
-  var lowerCaseValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  // Create variables for string values to be tied to user input choices
+  // Create variable to store all string values chosen by the user combined
+// Prompt user to select password length from 8 to 128
+// If user selects number between 8 and 128, move to next prompt
+// If user selects number not between 8-128, repeat select password length
+// Once length accepted, confirm user chooses upper case
+// Once length accepted, confirm user chooses lower case
+// Once length accepted, confirm user chooses number
+// Once length accepted, confirm user chooses special characters
+// Combine each true choice into one password generator var
+// Use random number generator to equal length of password generator string
+// Run it in loop until it generates the length equal to user's input for password length
+// Display user's password
+
+  var upperCaseValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCaseValues = "abcdefghijklmnopqrstuvwxyz";
   var numberValues = "1234567890";
   var specialValues = "!@#$%^&*()";
   var passwordGeneratorValue = "";
 
-  // var lengthOfPassword = prompt("Enter a number between 8 and 128");
-  // if (lengthOfPassword) {
+  
+  // ask TA in office hours
+    var lengthOfPassword = prompt("Enter a number between 8 and 128");
 
-  // }
-  // console.log("Password length is " + lengthOfPassword);
+    
+    // prompt("Enter a number between 8 and 128");
+    if (lengthOfPassword < 8 || lengthOfPassword > 128) {
+      alert("Please choose a number between 8 and 128")
+     return generatePassword ()
+  
+    }
+  
+
+  
+  console.log("Password length is " + lengthOfPassword);
 
   var useUpperCase = confirm("Would you like to use upper case letters?");
   if (useUpperCase) {
@@ -51,15 +76,26 @@ function generatePassword(){
     console.log("special characters confirmed");
   }
 
+  if (!useUpperCase && !useLowerCase && !useNumberValues && !useSpecialValues) {
+    alert("Please choose at least one");
+    generatePassword ()
+  }
+
   console.log("These values confirmed: " + passwordGeneratorValue);
 
+  
+  var password = ""
 
-  // for (var i = 0; i < lengthOfPassword; i++) {
+for (var i = 0; i < lengthOfPassword; i++) {
+  var randomIndex = Math.floor(Math.random() * passwordGeneratorValue.length);
+  var randomCharacter = passwordGeneratorValue[randomIndex];
+  password = randomCharacter + password;
+  console.log(password);
 
-  // }
 
+ }
 
-  return "temporary password";
+ return password;
 }
 
 // Add event listener to generate button
